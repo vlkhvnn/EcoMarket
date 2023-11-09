03ced3c5affc33bbd95db074b86466c93fb87322
Alikhan Tangirbergen <tanir.alikh@gmail.com>
2023-11-09 20:34:39 +0600

    CustomTextField

---
+++ EcoMarket/EcoMarket/Controllers/General/CustomAlertVC.swift
@@ -0,0 +1,20 @@
+//
+//  CustomAlertVC.swift
+//  EcoMarket
+//
+//  Created by Alikhan Tangirbergen on 09.11.2023.
+//
+
+import UIKit
+
+class CustomAlertVC: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
